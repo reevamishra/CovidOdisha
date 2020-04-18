5db9a17fa6cf507408978fd20a81925680ced01d
reevamishra <reevamishra208@gmail.com>
2020-04-18 11:02:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
-import * as Icon from 'react-feather';
+// import * as Icon from 'react-feather';
 
 import './App.scss';
 
 import Home from './components/home';
 import Navbar from './components/navbar';
-import Links from './components/links';
-import FAQ from './components/faq';
+// import Links from './components/links';
+// import FAQ from './components/faq';
 import Banner from './components/banner';
-import PatientDB from './components/patientdb';
-import DeepDive from './components/deepdive';
+// import PatientDB from './components/patientdb';
+// import DeepDive from './components/deepdive';
 import Resources from './components/resources';
 /* import PatientDB from './components/patientdb';*/
 
@@ -29,30 +29,30 @@ function App() {
       displayName: 'Home',
       animationDelayForNavbar: 0.2,
     },
-    {
-      pageLink: '/demographics',
-      view: PatientDB,
-      displayName: 'Demographics',
-      animationDelayForNavbar: 0.3,
-    },
-    {
-      pageLink: '/deepdive',
-      view: DeepDive,
-      displayName: 'Deep Dive',
-      animationDelayForNavbar: 0.4,
-    },
-    {
-      pageLink: '/links',
-      view: Links,
-      displayName: 'Helpful Links',
-      animationDelayForNavbar: 0.4,
-    },
-    {
-      pageLink: '/faq',
-      view: FAQ,
-      displayName: 'About',
-      animationDelayForNavbar: 0.5,
-    },
+//     {
+//       pageLink: '/demographics',
+//       view: PatientDB,
+//       displayName: 'Demographics',
+//       animationDelayForNavbar: 0.3,
+//     },
+//     {
+//       pageLink: '/deepdive',
+//       view: DeepDive,
+//       displayName: 'Deep Dive',
+//       animationDelayForNavbar: 0.4,
+//     },
+//     {
+//       pageLink: '/links',
+//       view: Links,
+//       displayName: 'Helpful Links',
+//       animationDelayForNavbar: 0.4,
+//     },
+//     {
+//       pageLink: '/faq',
+//       view: FAQ,
+//       displayName: 'About',
+//       animationDelayForNavbar: 0.5,
+//     },
     {
       pageLink: '/essentials',
       view: Resources,
@@ -95,52 +95,52 @@ function App() {
         />*/}
 
         <h5>We stand with everyone fighting on the frontlines</h5>
-        <div className="link">
-          <a
-            href="https://github.com/covid19india"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            covid19india
-          </a>
-        </div>
-        <a
-          href="https://github.com/covid19india/covid19india-react"
-          className="button github"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Icon.GitHub />
-          <span>Open Sourced on GitHub</span>
-        </a>
-        <a
-          className="button excel"
-          href="https://bit.ly/patientdb"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Icon.Database />
-          <span>Crowdsourced Patient Database&nbsp;</span>
-        </a>
-        <a
-          href="https://twitter.com/covid19indiaorg"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="button twitter"
-          style={{justifyContent: 'center'}}
-        >
-          <Icon.Twitter />
-          <span>View updates on Twitter</span>
-        </a>
-        <a
-          href="https://bit.ly/covid19crowd"
-          className="button telegram"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Icon.MessageCircle />
-          <span>Join Telegram to Collaborate!</span>
-        </a>
+//         <div className="link">
+//           <a
+//             href="https://github.com/covid19india"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             covid19india
+//           </a>
+//         </div>
+//         <a
+//           href="https://github.com/covid19india/covid19india-react"
+//           className="button github"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <Icon.GitHub />
+//           <span>Open Sourced on GitHub</span>
+//         </a>
+//         <a
+//           className="button excel"
+//           href="https://bit.ly/patientdb"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <Icon.Database />
+//           <span>Crowdsourced Patient Database&nbsp;</span>
+//         </a>
+//         <a
+//           href="https://twitter.com/covid19indiaorg"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           className="button twitter"
+//           style={{justifyContent: 'center'}}
+//         >
+//           <Icon.Twitter />
+//           <span>View updates on Twitter</span>
+//         </a>
+//         <a
+//           href="https://bit.ly/covid19crowd"
+//           className="button telegram"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <Icon.MessageCircle />
+//           <span>Join Telegram to Collaborate!</span>
+//         </a>
       </footer>
     </div>
   );
