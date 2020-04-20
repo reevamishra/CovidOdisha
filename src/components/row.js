54d71ef904b34cc96f2ae1e9ebd08c7f4bf492e9
reevamishra <reevamishra208@gmail.com>
2020-04-20 10:02:24 +0530

    Add files via upload

--- src/components/row.js
+++ src/components/row.js
@@ -121,7 +121,7 @@ function Row(props) {
         <td>
           <span className="deltas" style={{color: '#ff073a'}}>
             {state.deltaconfirmed > 0 && <Icon.ArrowUp />}
-            {state.deltaconfirmed > 0 ? `${state.deltaconfirmed}` : ''}
+            {state.deltaconfirmed > 0 ? `${t(state.deltaconfirmed)}` : ''}
           </span>
           <span className="table__count-text">
             {parseInt(state.confirmed) === 0
